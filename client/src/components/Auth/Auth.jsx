ccadcd37c42e93160da914a258df28125bdc7341
Kweebac <06.amarshall@gmail.com>
2024-03-28 12:36:48 +0000

    Fixed basic errors

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import "./auth.css";
+import "./Auth.css";
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useIsNotAuthenticated } from "../../Utilities";
--- client/src/components/Chat/MessageActions.jsx
+++ client/src/components/Chat/MessageActions.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
+import "./Chat.css";
 
 export default function MessageActions({
   chatId,
